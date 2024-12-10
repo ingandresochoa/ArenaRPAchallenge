7b3590516153f11280e70b3545454afba394b956
Andres Ochoa <andressochoaa@gmail.com>
2024-12-10 05:17:33 -0500

    clase para automatizacion con selenium

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select
 import pandas as pd
+import time
+
+class FormularioAutomatizado:
+    def __init__(self):
+        self.driver = webdriver.Chrome()
+        self.wait = WebDriverWait(self.driver, 10)
+    
+    def iniciar_navegador(self):
+        self.driver.get("https://arenarpa.com/crazy-form")
+        self.driver.maximize_window()
+        time.sleep(10)
+    
+    def cerrar_navegador(self):
+        self.driver.quit()
 
 def leer_datos_excel(ruta_archivo, nombre_hoja = 0):
     try:
@@ -22,6 +41,13 @@ def main():
 
     for index, fila in datos.iterrows():
         print(f"{fila['Nombres']} {fila['Apellidos']} - {fila['Empresa']} - {fila['Numero']} - {fila['Email']} - {fila['Pais']} - {fila['Web']}")
+
+    bot = FormularioAutomatizado()
+    try:
+        bot.iniciar_navegador()
+        bot.cerrar_navegador()
+    finally:
+        bot.cerrar_navegador()
 
 if __name__ == "__main__":
     main()
